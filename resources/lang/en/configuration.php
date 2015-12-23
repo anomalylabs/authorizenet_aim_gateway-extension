8f8afc3d100466396696bcd97dc42cdf28b6f3cb
Ryan Thompson <service@aiwebsystems.com>
2015-12-23 04:11:36 -0600

    Done

---
+++ resources/lang/en/configuration.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'test_mode'       => [
+        'name' => 'Mode'
+    ],
+    'api_login_id'    => [
+        'name' => 'API Login ID'
+    ],
+    'transaction_key' => [
+        'name' => 'Transaction Key'
+    ]
+];
